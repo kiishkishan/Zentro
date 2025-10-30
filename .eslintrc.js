71f99c4eb722641107aec632d9faa5bfbfe77c31
kishanth1 <kishanth@apptimustech.com>
2025-10-30 21:35:54 +0530

    Initial setup, architecture and package installations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // Recommended tweaks for RN Production
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-unused-styles': 'warn',
+    'no-unused-vars': 'warn',
+  },
 };
