dbed217b5c9328cc0f046d7c07c60b3d0cd9a757
kishanth1 <kishanth@apptimustech.com>
2025-10-31 06:22:45 +0530

    Refactor App component structure add CartContext for state management Linted all the files

--- App.tsx
+++ App.tsx
@@ -1,16 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React from 'react';
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { store } from './src/store/store';
+import { CartProvider } from './src/context/CartContext';
+import AppNavigation from './src/navigations/AppNavigations';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -24,22 +18,13 @@ function App() {
 }
 
 function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+    <Provider store={store}>
+      <CartProvider>
+        <AppNavigation />
+      </CartProvider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
