39531b69b95a543e3e8a4cb9bff057e4d66c18f2
kishanth1 <kishanth@apptimustech.com>
2025-11-01 02:58:52 +0530

    theme : enhance color palette and add typography - Added new colors - Updated index.ts to include typography module. - Created typography.ts with font weight definitions.

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -5,14 +5,22 @@ const colors = {
 
   background: '#E7E7E7',
   surface: '#FFFFFF',
+  base: '#dbdbdbff',
+  shadow: '#96969633',
 
   text: '#153A5A',
   textSecondary: '#4F4F4F',
   textMuted: '#9CA3AF',
+  textSemiMuted: '#6c6c70',
+  textTitle: '#1c1c1e',
 
   border: '#E5E7EB',
   error: '#E63946',
   success: '#00C9A7',
+
+  // mandatory colors
+  white: '#FFFFFF',
+  black: '#000000',
 };
 
 export default colors;
